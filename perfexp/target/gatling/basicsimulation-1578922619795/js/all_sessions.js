39dd262a86e7193540915c340b172f0f4ffc864f
abhinandan baheti <abaheti@vmware.com>
2020-01-13 19:11:12 +0530

    Basic simulation perf test

---
+++ perfexp/target/gatling/basicsimulation-1578922619795/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'All Users',
+data: [
+  [1578922621000,1],[1578922622000,1],[1578922623000,1],[1578922624000,1],[1578922625000,1],[1578922626000,1],[1578922627000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
